abb8ec42bb61a71c4cf698be0b3f413ca8502be2
Carlos Milián <carlitosmilianpinelo@gmail.com>
2021-05-31 16:57:08 -0600

    Versio final a presentar del proyecto

--- bibliotecaVersion2.js
+++ bibliotecaVersion2.js
@@ -24,8 +24,9 @@ Estas respuestas serían las que devolvería el programa cuando detecte de que s
 Estas mismas también son traducidas al idioma en el que está escrita la emergencia en la petición incial. 
 Por ahora solo hay cuatro respuestas para emergencias específicas." */
 var arrayIncendio = ['fuego', "incendio", "humo", "amigo", "casa"];
-var arrayTiroteo =  ["amigo", "casa", "balacera", "tiroteo", "balazo", "bala", "pistola", "estomago", "brazo", "Tiroteo escolar",];
-var arrayHerida = ["higado", "corte", "balazo", "disparo", "tiroteo", "pierna", "cabeza", "amigo", "herido"];
+var arrayTiroteo =  ["amigo", "casa", "balacera", "tiroteo", "balazo", "bala", "pistola",
+                     "estomago", "brazo", "Tiroteo escolar", "hay una balacera en", "tirar"];
+var arrayHerida = ["higado", "corte", "balazo", "disparo", "tiroteo", "pierna", "cabeza", "amigo", "herido", "hirieron", "herida"];
 exports.palabraClaveIncendio = (index) =>{
     return arrayIncendio[index];
 }
@@ -38,7 +39,7 @@ exports.palabraClaveHerida = (index) => {
 exports.find = (index) =>{
     switch (index) {
         case 0:
-            return "Si se encuentra atrapado dentro de su hogar y no puede salir, diijase a un lugar visible desde fuera " +
+            return "Si se encuentra atrapado dentro de su hogar y no puede salir, dirijase a un lugar visible desde fuera " +
     "Cierre las puertas que pueda a su paso hasta llegar al lugar indicado. Si puede, bloquee "+
     "la rendija de la puerta con una tela mojada para que no pase el humo. "+
     "Usted también puede taparse nariz y boca con una tela mojada para evitar el humo "+
@@ -48,15 +49,6 @@ exports.find = (index) =>{
     "Si la parte alta del edificio se está incendiando, si puede, recoja las llaves de su departamento "+
     "y baje por las escaleras, o salga de su domicilio. ";
         case 1:
-            return "Manten la calma, si estas acompañado tranquiliza al que te rodea, "+ 
-    "por nada del mundo opongas resistencia, haz lo que te digan y como te lo digan, " +
-    "evita el contacto visual. Que el asaltante sienta que tiene el control "+
-    "y que no hay motivo para agredirte. "+
-    "Tus movimientos deben ser lentos y antes de hacerlos debes avisarle al asaltante con voz suave y baja. "+
-    "Presta atención a los detalles, a su acento, alguna cicatriz o marca (no lo estudies, solo presta atención) "+
-    "Como último recurso, si la situación se torna violenta por algún motivo finje un desmayo. "+
-    "Oculta tu teléfono y resiste, la ayuda llegará pronto. ";
-        case 2:
             return "Serenate y manten la calma, tu raciocinio es tu mejor recurso en estos casos. Lo primero es que evites "+
     "congelarte por el miedo, si estas atrapado dentro de una balacera en algún lugar público lo más "+
     "seguro es que existan rutas de escape, si la conoces, intenta utilizarlas. Sin embargo, si seguir esas "+
@@ -75,23 +67,8 @@ exports.find = (index) =>{
     "Un torniquete lo más cerca de la herida pero siempre arriba, los torniquetes pueden detener la hemorragia "+
     "pero pueden causar daño, afloja el torniquete cada tanto para revisar el estado de la herida. Tranquilizate"+
     "la ayuda no tardará en llegar.";
-
-        case 3:
-            return "Mantener la calma es primordial en estas situaciones, no debes dejarte ganar por el miedo. Lo primero es: "+
-    "si tienes la posibilidad, huir del lugar. Las escuelas tienen rutas de escape y si las conoces entonces "+
-    "puedes usarlas para salir. Sin embargo, si las salidas no son seguras seguras entonces debes "+
-    "pasar de elas, lo mejor en esos casos será buscar un escondite, cualquiera que en el que te puedas refugiar "+
-    "tú, o tus compañeos sirve. Una vez lo encuentres, cierra la puerta y bloqueala con lo que tengas y luego aléjate "+
-    "mantente escondido debajo de un escritorio o mesa lo más alejado posible de las ventanas y minimizar tu tamaño "+
-    "lo más qu puedas, si te es posible es recomendable que te acuestes en el piso. Luchar contra el tirador es el "+
-    "último recurso y solo lo debes hacer cuando tu vida corre verdadero pelígro, intenta lanzarle cosas o usar objetos "+
-    "filosos o contundentes como armas, si estas acompañado, todos deben sumarse. Si estás herido aplica presión con "+
-    "una tela o una gasa en el orificio de la herida, conserva la calma, tratar una herida de bala es delicado y emperoa si "+
-    "te alteras. Si la hemorragia no para con la presión, y si la herida es en una extremidad, puedes aplicar un torniquete "+
-    "lo más cerca de la herida pero siempre arriba. Los torniquetes pueden ocasionar daños, aflojalos cada tanto para "+
-    "verificar que la hemorragia ha parado. Tranquilizate, la ayuda no tardará en llegar."
     break;
-    case 4:
+    case 2:
         return "Si la herida está sangrando profusamente, llame al número local de emergencias, como el 911 (en los Estados Unidos)" +
         "Las heridas menores y las punciones se pueden tratar en casa. Proporcionar los primeros auxilios de manera oportuna puede " 
         + "ayudar a prevenir una infección, y por lo tanto, acelera la curación y reduce la cantidad de cicatrices."
